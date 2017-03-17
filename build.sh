22d2a76f316127328d37529d5e683c3213169178
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-03-16 18:54:02 -0600

    Removing big files

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ fi
 rm build/* -r
 
 ./node_modules/.bin/electron-packager ./ --platform linux --arch x64 --icon images/icon.png --out build/
+./node_modules/.bin/electron-packager ./ --platform darwin --arch x64 --icon images/icon.png --out build/
 
 cd build
 
-
 tar -cvzf timetracker-linux-x64.tar.gz timetracker-linux-x64
 zip -r timetracker-linux-x64.zip timetracker-linux-x64/*
+zip -r timetracker-darwin-x64.zip timetracker-darwin-x64/*
