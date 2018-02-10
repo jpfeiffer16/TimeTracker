e1a44719461614d73d64dbc3343f76e18b51abf6
Joe Pfeiffer <jpfeiffer@bluemodus.com>
2018-02-09 23:07:26 -0700

    Try again

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ New-Item -ItemType Directory -Force -Path .\build
 .\node_modules\.bin\electron-packager.cmd .\ --platform win32 --arch x64 --icon images\icon.png --out build
 
 
-Compress-Archive -Update -Path .\build\timetracker-win32-x64 -DestinationPath .\build\timetracker-win32-x64.zip
+# Compress-Archive -Update -Path .\build\timetracker-win32-x64 -DestinationPath .\build\timetracker-win32-x64.zip
