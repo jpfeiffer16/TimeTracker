8f56214f7e05f3bf37d4da94d160545fdb8b52b2
jpfeiffer16 <joepfeiffer16@gmail.com>
2018-03-12 20:41:51 -0600

    Got build working again

--- build.sh
+++ build.sh
@@ -6,16 +6,22 @@ fi
 
 rm build/* -r
 
-cd client
+#Server
+echo "Building server"
+cd server_native
+cargo build --release
+cd ..
 
+#Client
+echo "Building client"
+cd client
 ./node_modules/.bin/electron-packager ./ --platform linux --arch x64 --icon images/icon.png --out ../build/
 ./node_modules/.bin/electron-packager ./ --platform win32 --arch x64 --icon images/icon.png --out ../build/
 
-cp ./.env-prod ../build/timetracker-linux-x64/resources/app/.env
-
-cp ../server_native/target/debug/server_native ../build/timetracker-linux-x64/resources/app/
-
+#Create final package
+echo "Creating final package"
+cp ./.env-prod ../build/timetracker-app-linux-x64/resources/app/.env
+cp ../server_native/target/release/server_native ../build/timetracker-app-linux-x64/resources/app/
 cd ../build
-
-tar -cvzf timetracker-linux-x64.tar.gz timetracker-linux-x64
-zip -r timetracker-win32-x64.zip timetracker-win32-x64/*
+tar -cvzf timetracker-app-linux-x64.tar.gz timetracker-app-linux-x64
+zip -r timetracker-app-win32-x64.zip timetracker-app-win32-x64/*
